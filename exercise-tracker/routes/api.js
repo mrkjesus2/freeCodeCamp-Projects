157c3f6853af06d813e53f2843e417d2ea404c1b
Mark Weigel <markweig@gmail.com>
2020-06-26 18:01:10 -0400

    Move api route to routes/api.js

---
+++ exercise-tracker/routes/api.js
@@ -0,0 +1,69 @@
+const user = require('../models/user')
+const mongoose = require('mongoose')
+const route = require('express').Router()
+
+route.get('/log:userId?:from?:to?:limit?', (req, res, next) => {
+    let fromDate = req.query.from ? new Date(req.query.from) : new Date('1920-1-1')
+    let toDate = req.query.to ? new Date(req.query.to) : new Date()
+  
+    user.aggregate([
+          { $match: {'_id': mongoose.Types.ObjectId(req.query.userId)} },
+          { $match: {'exercises.date': {$gte: fromDate, $lte: toDate}} }, 
+          { $unwind: '$exercises'}, 
+          { $match: {'exercises.date': {$gte: fromDate, $lte: toDate}} }
+        ])
+        .project({
+          _id: 0,
+          desc: '$exercises.description', 
+          duration: '$exercises.duration', 
+          date: '$exercises.date'
+        })
+        .then(async result => {
+          let arr = result.slice(0, req.query.limit || result.length)
+          let resObj = {
+            userId: req.query.userId,
+            count: result.length,
+            log: result
+          }
+          res.json(resObj)
+        })
+        .catch(err => next(err))
+  })
+  
+  route.get('/users', (req, res, next) => {
+    user.find({}, (err, people) => {
+      if (err) next(err)
+      let users = []
+      for (person in people) {
+        let curr = people[person]
+        users.push({username: curr.name, _id: curr._id})
+      }
+      res.json({users})
+    })
+  })
+  
+  route.post('/new-user', (req, res, next) => {
+    let newUser = new user({name: req.body.username})
+    
+    newUser.save()
+          .then(d => res.json({username: d.name, _id: d._id}))
+          .catch(e => next(e))
+  })
+  
+  
+  route.post('/add', (req, res, next) => {
+    user.findById(req.body.userId, (err, currUser) => {
+      if (err) next(err)
+      let date = req.body.date
+      currUser.exercises.push({
+            description: req.body.description,
+            duration: req.body.duration,
+            date: date ? new Date(req.body.date) : new Date()
+      })
+      currUser.save()
+              .then(d => res.json(d))
+              .catch(err => next(err))
+    })
+  })
+
+  module.exports = route
--- exercise-tracker/server.js
+++ exercise-tracker/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const apiRoutes = require('./routes/api')
 require('dotenv').config()
 
-const user = require('./models/user')
 
 const cors = require('cors')
 
@@ -21,69 +21,8 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-app.get('/api/exercise/log:userId?:from?:to?:limit?', (req, res, next) => {
-  let fromDate = req.query.from ? new Date(req.query.from) : new Date('1920-1-1')
-  let toDate = req.query.to ? new Date(req.query.to) : new Date()
+app.use('/api/exercise', apiRoutes)
 
-  user.aggregate([
-        { $match: {'_id': mongoose.Types.ObjectId(req.query.userId)} },
-        { $match: {'exercises.date': {$gte: fromDate, $lte: toDate}} }, 
-        { $unwind: '$exercises'}, 
-        { $match: {'exercises.date': {$gte: fromDate, $lte: toDate}} }
-      ])
-      .project({
-        _id: 0,
-        desc: '$exercises.description', 
-        duration: '$exercises.duration', 
-        date: '$exercises.date'
-      })
-      .then(async result => {
-        let arr = result.slice(0, req.query.limit || result.length)
-        let resObj = {
-          userId: req.query.userId,
-          count: result.length,
-          log: result
-        }
-        res.json(resObj)
-      })
-      .catch(err => next(err))
-})
-
-app.get('/api/exercise/users', (req, res, next) => {
-  user.find({}, (err, people) => {
-    if (err) next(err)
-    let users = []
-    for (person in people) {
-      let curr = people[person]
-      users.push({username: curr.name, _id: curr._id})
-    }
-    res.json({users})
-  })
-})
-
-app.post('/api/exercise/new-user', (req, res, next) => {
-  let newUser = new user({name: req.body.username})
-  
-  newUser.save()
-        .then(d => res.json({username: d.name, _id: d._id}))
-        .catch(e => next(e))
-})
-
-
-app.post('/api/exercise/add', (req, res, next) => {
-  user.findById(req.body.userId, (err, currUser) => {
-    if (err) next(err)
-    let date = req.body.date
-    currUser.exercises.push({
-          description: req.body.description,
-          duration: req.body.duration,
-          date: date ? new Date(req.body.date) : new Date()
-    })
-    currUser.save()
-            .then(d => res.json(d))
-            .catch(err => next(err))
-  })
-})
 
 // Not found middleware
 app.use((req, res, next) => {
