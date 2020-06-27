dabd91cd6d057c77f0df20f608ee040e243e488c
Mark Weigel <markweig@gmail.com>
2020-06-27 13:25:08 -0400

    Add file upload handling to /api/fileanalyse route

--- file-metadata/server.js
+++ file-metadata/server.js
@@ -2,8 +2,9 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var multer = require('multer');
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
 
 var app = express();
 
@@ -14,8 +15,13 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
+  console.log(req.file)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
 });
 
 app.listen(process.env.PORT || 3000, function () {
