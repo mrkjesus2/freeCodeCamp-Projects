1b12d885c1bb952e6ee01965c9df0f6a6818c98e
Mark Weigel <markweig@gmail.com>
2020-07-07 11:22:01 -0400

    Add helmet to metric-converter

--- metric-converter/server.js
+++ metric-converter/server.js
@@ -5,6 +5,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+var helmet      = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -15,6 +16,8 @@ var app = express();
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
+
+app.use(helmet())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
