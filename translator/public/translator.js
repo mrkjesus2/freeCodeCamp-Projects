1348994e9473212b2182d0979bbc607c8024ecda
Mark Weigel <markweig@gmail.com>
2020-07-14 12:28:21 -0400

    Add unit tests to translator

---
+++ translator/public/translator.js
@@ -0,0 +1,26 @@
+import { americanOnly } from './american-only.js';
+import { britishOnly } from './british-only.js';
+import { americanToBritishSpelling } from './american-to-british-spelling.js';
+import { americanToBritishTitles } from './american-to-british-titles.js';
+
+/* 
+  Export your functions for testing in Node.
+  Note: The `try` block is to prevent errors on
+  the client side
+*/
+let translator = function() {
+
+  return ({
+    toBritish: function() {
+      console.log('Translating to British')
+    },
+
+    toAmerican: function() {
+      console.log('Translating to American')
+    }
+  })
+}
+
+try {
+  module.exports = translator()
+} catch (e) {}
