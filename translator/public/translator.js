0a4479bf46a6cc5f11baec3173088d4ffac13043
Mark Weigel <markweig@gmail.com>
2020-07-17 16:38:59 -0400

    Rewrite translator.js - See what the performance difference would be

--- translator/public/translator.js
+++ translator/public/translator.js
@@ -22,79 +22,96 @@ document.addEventListener('DOMContentLoaded', ev => {
   select.addEventListener('input', ev => {
     //Change the language setting
     translator.lang = options[options.selectedIndex].value
-    console.log(translator.lang)
   })
 
   // Initialize the language setting
   translator.lang = options[options.selectedIndex].value
 })
 
-
 /* 
   Export your functions for testing in Node.
   Note: The `try` block is to prevent errors on
   the client side
 */
-let translator = (function() {
+let translator = (() => {
   let timeRegex = /\d+[:.]\d+/
+  let britishToAmericanSpelling = createReverseAssociation(americanToBritishSpelling)
+  let britishToAmericanTitles = createReverseAssociation(americanToBritishTitles)
 
-  // Create British Spelling object
-  let britishToAmericanSpelling = {}
-  createReverseAssociation(americanToBritishSpelling, britishToAmericanSpelling)
-  
-  //Create British Title object
-  let britishToAmericanTitles = {}
-  createReverseAssociation(americanToBritishTitles, britishToAmericanTitles)
-
-  console.log(britishToAmericanSpelling.caramelised)
   /**
-   * 
+   * Returns an object with propeties and values reversed
    * @param {Object} origObj | Object with associative translations in prop/val
-   * @param {Object} newObject | Empty object that will contain reverse associations
    */
-  function createReverseAssociation(origObj, newObject) {
+  function createReverseAssociation(origObj) {
+    let newObj = {}
     for (const word in origObj) {
       if (origObj.hasOwnProperty(word)) {
         const translated = origObj[word];
-        newObject[translated] = word
+        newObj[translated] = word
       }
     }
+    return newObj
+  }
+ 
+  function getAllCombos(str) {
+    let arr = str.split(' ')
+    let combos = []
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = i + 1; j <= arr.length; j++) {
+        let curr = arr.slice(i, j).join(' ')
+        if (!combos.includes(curr)) {
+          combos.push(curr)
+        }
+      }
+    }
+
+    let sorted = combos.sort((a,b) => b.length - a.length)
+
+    return sorted
+  }
+
+  function replace(phraseArr, obj) {
+    // console.log('REPLACE')
+    // let newPhrase = str
+    // let phraseArr = getAllCombos(str)
+
+    phraseArr.forEach(phrase => {
+      let stripped = !americanToBritishTitles[phrase.toLowerCase] === phrase.toLowerCase()
+                     || !americanToBritishTitles[phrase.toLowerCase()]
+                        ? phrase.replace(/[.?]/, '')
+                        : phrase.replace('?', '')
+      let lower = stripped.toLowerCase()
+
+      if (obj[lower]) {
+        // Handle Capitalization
+        let translation = obj[lower].split(' ')
+        let words = phrase.split(' ')
+        
+        phrase.split(' ').forEach((word, idx) => {
+          if (isCapitalized(word) && 
+              words.length === translation.length) {
+                translation[idx] = capitalize(translation[idx])
+          }
+        })
+
+        // Replace phrase with translation
+        // newPhrase = newPhrase.replace(stripped, translation.join(' '))
+        phraseArr.forEach((oldPhrase, idx) => {
+          // console.log('OLD', oldPhrase)
+          phraseArr[idx] = oldPhrase.replace(stripped, wrap(translation.join(' ')))
+          // console.log('NEW', oldPhrase)
+        })
+      }
+    })
+    // console.log(phraseArr)
+    return phraseArr
   }
 
   /**
-   * Returns a boolean - True if capitalized, False if not
-   * @param {String} word | Word to determine capitalization
+   * Converts time separator from : to . and vice versa
+   * @param {String} str | The time to convert
    */
-  function isCapitalized(word) {
-    let firstLtr = word[0]
-    return firstLtr === firstLtr.toUpperCase() ? true : false
-  }
-
-  function capitalize(word) {
-    let first = word[0].toUpperCase()
-    let rest = word.split('').slice(1).join('')
-
-    return first + rest
-  }
-
-  function wrap(str) {
-    return `<span class="highlight">${str}</span>`
-  }
-
-  function replacePhrase(str, orgPhrase, newPhrase) {
-    let newSentence
-    let idx = str.toLowerCase().indexOf(orgPhrase)
-    let wasCapitalized = isCapitalized(str[idx])
-
-    if (wasCapitalized) {
-      newSentence = str.replace(capitalize(str.slice(idx, idx + orgPhrase.length)), wrap(capitalize(newPhrase)) )
-    } else {
-      newSentence = str.replace(orgPhrase, wrap(newPhrase))
-    }
-
-    return newSentence
-  }
-
   function replaceTime(str) {
     let prevTime = str.match(timeRegex)
     let newTime
@@ -109,115 +126,57 @@ let translator = (function() {
   }
 
   /**
-   * 
-   * @param {String} str | String to search for titles to replace
-   * @param {Object} obj | Object containing associated values for titles
-   */
-  function replaceTitle(str, obj) {
-    let newStr = str
-
-    for (const title in obj) {
-      if (obj.hasOwnProperty(title)) {
-        if (~str.toLowerCase().indexOf(title.toLowerCase())) {
-          const newTitle = obj[title];
-          newStr = replacePhrase(str, title, newTitle )    
-        }
-      }
-    }
-
-    return newStr
+  * Returns a boolean - True if capitalized, False if not
+  * @param {String} word | Word to determine capitalization
+  */
+  function isCapitalized(word) {
+    if (!word) { return false }
+    let firstLtr = word[0]
+    return firstLtr === firstLtr.toUpperCase() ? true : false
   }
 
-  /**
-   * 
-   * @param {String} str | String to look for words that need replacing
-   * @param {Object} obj | An object with the translation associative object
-   */
-  function replaceSpelling(str, obj) {
-    let newStr = str
-    for (const word in obj) {
-      if (obj.hasOwnProperty(word)) {
-        if (~str.toLowerCase().indexOf(word)) {
-          const translation = obj[word];
-          // newStr = str.replace(word, translation)
-          newStr = replacePhrase(str, word, wrap(translation) )
-        }
-      }
-    }
-    return newStr
+  function capitalize(word) {
+    let first = word[0].toUpperCase()
+    let rest = word.split('').slice(1).join('')
+
+    return first + rest
   }
-
-  function findSimilarKeys(str, obj) {
-    let keys = Object.keys(obj)
-    let similar = keys.filter(key => {
-      let words = key.split(' ')
-      let hasAll = true
-
-      words.forEach(word => {
-        if (str.toLowerCase().indexOf(word.toLowerCase()) === -1 && words.length > 1) { 
-          hasAll = false 
-        }
-      });
-      return hasAll && key.includes(str.toLowerCase())
-    })
-
-    return similar
+  
+  function wrap(str) {
+    return `<span class="highlight">${str}</span>`
   }
-
-  /**
-   * 
-   * @param {String} str | String that has words to replace
-   * @param {Object} obj | Object with associative object of translations
-   */
-  function replaceSlang(str, obj) {
-    let newStr = str
-    let regex = /[\w-]/ 
-
-    for (const saying in obj) {
-      if (obj.hasOwnProperty(saying)) {
-        const translation = obj[saying];
-        let idx = newStr.toLowerCase().indexOf(saying)
-
-        let similarKeys = findSimilarKeys(saying, obj)
-
-        // Check for preceding space unless it's the first word
-        let shouldReplace = idx === 0 || !regex.test(newStr[idx - 1])
-
-        if (~idx && similarKeys.length == 1 && shouldReplace) {
-          newStr = replacePhrase(newStr, saying, wrap(translation) )
-        } 
-      }
-    }
-
-    return newStr
-  }
-
-
-  return ({
+  
+  return {
     lang: '',
 
     toBritish: function(str) {
-      let newStr = str
-      
-      newStr = replaceTime(newStr)
-      newStr = replaceTitle(newStr, americanToBritishTitles)
-      newStr = replaceSlang(newStr, americanOnly)
-      newStr = replaceSpelling(newStr, americanToBritishSpelling)
-      
-      // console.log('TEST', newStr)
-      return newStr
+      // console.log('TO BRITISH')
+      let t0 = new Date().getMilliseconds()
+      let newStr = replaceTime(str)
+      let combos = getAllCombos(newStr)
+
+      let slang = replace(combos, americanOnly)
+      let spelling = replace(slang, americanToBritishSpelling)
+      let final = replace(spelling, americanToBritishTitles)
+
+      let t1 = new Date().getMilliseconds()
+      console.log('Time:', t1 - t0)
+      console.log('Final', final[0])
+      return final[0]
     },
 
     toAmerican: function(str) {
-      let newStr = str
+      let t0 = new Date().getMilliseconds()
+      let newStr = replaceTime(str)
+      let combos = getAllCombos(newStr)
 
-      newStr = replaceTime(newStr)      
-      newStr = replaceTitle(newStr, britishToAmericanTitles)
-      newStr = replaceSlang(newStr, britishOnly)
-      newStr = replaceSpelling(newStr, britishToAmericanSpelling)      
-      
-      // console.log('TEST', newStr)
-      return newStr
+      let slang = replace(combos, britishOnly)
+      let spelling  = replace(slang, britishToAmericanSpelling)
+      let final = replace(spelling, britishToAmericanTitles)
+
+      let t1 = new Date().getMilliseconds()
+      // console.log('Time:', t1 - t0)
+      return final[0]
     },
 
     clearInput: function() {
@@ -236,13 +195,10 @@ let translator = (function() {
       let translated
 
       if (text === '') {
-        console.log('Showing error')
         this.showError('Error: No text to translate.')
       } else if (this.lang === 'american-to-british') {
-        console.log('To British')
         translated = this.toBritish(text)
       } else {
-        console.log('To American')
         translated = this.toAmerican(text)
       }
     
@@ -257,14 +213,277 @@ let translator = (function() {
     showError: function(msg) {
       let el = document.getElementById('error-msg')
       el.append(`<p>${msg}</p>`)
-    },
-
-    clearError: function() {
-      
     }
-
-  })
+    
+  }
 })()
+
+// let translator = (function() {
+//   let timeRegex = /\d+[:.]\d+/
+//   let britishToAmericanSpelling = createReverseAssociation(americanToBritishSpelling)
+//     let britishToAmericanTitles = createReverseAssociation(americanToBritishTitles)
+
+//   /**
+//    * Returns an object with propeties and values reversed
+//    * @param {Object} origObj | Object with associative translations in prop/val
+//    */
+//   function createReverseAssociation(origObj) {
+//     let newObj = {}
+//     for (const word in origObj) {
+//       if (origObj.hasOwnProperty(word)) {
+//         const translated = origObj[word];
+//         newObj[translated] = word
+//       }
+//     }
+//     return newObj
+//   }
+
+//   /**
+//    * Returns a boolean - True if capitalized, False if not
+//    * @param {String} word | Word to determine capitalization
+//    */
+//   function isCapitalized(word) {
+//     if (!word) { return false }
+//     let firstLtr = word[0]
+//     return firstLtr === firstLtr.toUpperCase() ? true : false
+//   }
+
+//   function capitalize(word) {
+//     let first = word[0].toUpperCase()
+//     let rest = word.split('').slice(1).join('')
+
+//     return first + rest
+//   }
+
+//   function wrap(str) {
+//     return `<span class="highlight">${str}</span>`
+//   }
+
+//   /**
+//    * Case sensitve replacement of given phrase
+//    * Outputs new phrase with replacements wrapped in
+//    * <span class="highlight">[newPhrase]</span
+//    * 
+//    * @param {String} str | The string containing words to replace
+//    * @param {String} orgPhrase | The phrase to replace
+//    * @param {String} newPhrase | The phrase to include
+//    */
+//   function replacePhrase(str, orgPhrase, newPhrase) {
+//     let newSentence
+//     let idx = str.toLowerCase().indexOf(orgPhrase)
+//     let wasCapitalized = isCapitalized(str[idx])
+
+//     if (wasCapitalized) {
+//       newSentence = str.replace(capitalize(str.slice(idx, idx + orgPhrase.length)), wrap(capitalize(newPhrase)) )
+//     } else {
+//       newSentence = str.replace(orgPhrase, wrap(newPhrase))
+//     }
+
+//     return newSentence
+//   }
+
+//   /**
+//    * Converts time separator from : to . and vice versa
+//    * @param {String} str | The time to convert
+//    */
+//   function replaceTime(str) {
+//     let prevTime = str.match(timeRegex)
+//     let newTime
+
+//     if (prevTime) {
+//       newTime = ~prevTime[0].indexOf('.') 
+//                 ? prevTime[0].replace('.', ':') 
+//                 : prevTime[0].replace(':', '.')
+//     }
+      
+//     return prevTime ? str.replace(prevTime[0], wrap(newTime) ) : str
+//   }
+
+//   /**
+//    * 
+//    * @param {String} str | String to search for titles to replace
+//    * @param {Object} obj | Object containing associated values for titles
+//    */
+//   function replaceTitle(str, obj) {
+//     let newStr = str
+
+//     for (const title in obj) {
+//       if (obj.hasOwnProperty(title)) {
+//         if (~str.toLowerCase().indexOf(title.toLowerCase())) {
+//           const newTitle = obj[title];
+//           newStr = replacePhrase(str, title, newTitle )    
+//         }
+//       }
+//     }
+
+//     return newStr
+//   }
+
+//   /**
+//    * 
+//    * @param {String} str | String to look for words that need replacing
+//    * @param {Object} obj | An object with the translation associative object
+//    */
+//   function replaceSpelling(str, obj) {
+//     let newStr = str
+//     for (const word in obj) {
+//       if (obj.hasOwnProperty(word)) {
+//         if (~str.toLowerCase().indexOf(word)) {
+//           const translation = obj[word];
+//           newStr = replacePhrase(newStr, word, wrap(translation) )
+//         }
+//       }
+//     }
+//     return newStr
+//   }
+
+//   let similarKeys = findSimilar()
+
+//   function findSimilar() {
+//     let objects = [
+//       americanOnly,
+//       britishOnly,
+//       americanToBritishSpelling,
+//       britishToAmericanSpelling
+//     ]
+//     let similar = []
+//     let keysLength = 0
+//     objects.forEach(obj => {
+//       let arr = []
+//       let keys = Object.keys(obj)
+//       keysLength += keys.length
+      
+//       keys.forEach(key => {
+//         keys.forEach(key2 => {
+//           if (~key.indexOf(key2) && key !== key2) {
+//             arr.push(key)
+//             arr.push(key2)
+//           }
+//         })
+//       })
+//       similar = [...similar, ...arr]
+//     })
+//     console.log('INFO', similar.length, keysLength)
+//     return similar
+//   }
+//   // console.log('Similar', similarKeys)
+
+//   function findSimilarKeys(str, obj) {
+//     let keys = Object.keys(obj)
+//     let similar = keys.filter(key => {
+//       let words = key.split(' ')
+//       let hasAll = true
+
+//       words.forEach(word => {
+//         if (str.toLowerCase().indexOf(word.toLowerCase()) === -1 && words.length > 1) { 
+//           hasAll = false 
+//         }
+//       });
+//       return hasAll && key.includes(str.toLowerCase())
+//     })
+//     // console.log(similar)
+//     return similar
+//   }
+
+//   /**
+//    * 
+//    * @param {String} str | String that has words to replace
+//    * @param {Object} obj | Object with associative object of translations
+//    */
+//   function replaceSlang(str, obj) {
+//     let newStr = str
+//     let regex = /[\w-]/ 
+
+//     for (const saying in obj) {
+//       if (obj.hasOwnProperty(saying)) {
+//         const translation = obj[saying];
+//         let idx = newStr.toLowerCase().indexOf(saying)
+
+//         // Check for preceding space unless it's the first word
+//         let shouldReplace = idx === 0 || !regex.test(newStr[idx - 1])
+
+//         let similarKeys = findSimilarKeys(saying, obj)
+
+//         if (~idx && similarKeys.length == 1 && shouldReplace) {
+//           newStr = replacePhrase(newStr, saying, wrap(translation) )
+//         }
+//       }
+//     }
+
+//     return newStr
+//   }
+
+
+//   return ({
+//     lang: '',
+
+//     toBritish: function(str) {
+//       let t0 = new Date().getMilliseconds()
+//       let newStr = str
+      
+//       newStr = replaceTime(newStr)
+//       newStr = replaceTitle(newStr, americanToBritishTitles)
+//       newStr = replaceSlang(newStr, americanOnly)
+//       newStr = replaceSpelling(newStr, americanToBritishSpelling)
+      
+//       let t1 = new Date().getMilliseconds()
+//       console.log('Time:', t1 - t0)
+//       return newStr
+//     },
+
+//     toAmerican: function(str) {
+//       let newStr = str
+
+//       newStr = replaceTime(newStr)      
+//       newStr = replaceTitle(newStr, britishToAmericanTitles)
+//       newStr = replaceSlang(newStr, britishOnly)
+//       newStr = replaceSpelling(newStr, britishToAmericanSpelling)      
+      
+//       return newStr
+//     },
+
+//     clearInput: function() {
+//       const errMsg = document.getElementById('error-msg')
+//       const phrase = document.getElementById('translated-sentence')
+//       const textArea = document.getElementById('text-input')
+
+//       errMsg.innerHTML = ''
+//       phrase.innerHTML = ''
+//       textArea.value = ''
+//     },
+
+//     showTranslation: function(text) {
+//       this.clearInput()
+//       let output = document.getElementById('translated-sentence')
+//       let translated
+
+//       if (text === '') {
+//         this.showError('Error: No text to translate.')
+//       } else if (this.lang === 'american-to-british') {
+//         translated = this.toBritish(text)
+//       } else {
+//         translated = this.toAmerican(text)
+//       }
+    
+//       if (translated === text) {
+//         translated = 'Everything looks good to me!'
+//       }
+//       if (translated) {
+//         output.innerHTML = translated
+//       }
+//     },
+
+//     showError: function(msg) {
+//       let el = document.getElementById('error-msg')
+//       el.append(`<p>${msg}</p>`)
+//     },
+
+//     clearError: function() {
+      
+//     }
+
+//   })
+// })()
 
 try {
   module.exports = translator
--- translator/tests/1_unit-tests.js
+++ translator/tests/1_unit-tests.js
@@ -2,7 +2,7 @@
  *
  *
  *       FILL IN EACH UNIT TEST BELOW COMPLETELY
- *       -----[Keep the test.skips in the same order!]----
+ *       -----[Keep the tests in the same order!]----
  *       (if additional are added, keep them at the very end!)
  */
 
@@ -12,33 +12,30 @@ const assert = chai.assert;
 const { JSDOM } = require('jsdom');
 let Translator;
 let wrapReg = /<[\w\s="/]*>/g
-let wrap1 = '<span class="highlight">'
-let wrap2 = '</span>'
 
 suite('Unit Tests', () => {
   suiteSetup(() => {
-    // Mock the DOM for test.skiping and load Translator
+    // Mock the DOM for testing and load Translator
     return JSDOM.fromFile('./views/index.html', {runScripts: 'dangerously'})
       .then((dom) => {
         global.window = dom.window;
         global.document = dom.window.document;
 
         Translator = require('../public/translator.js');
-        console.log(Translator)
       });
   });
 
   suite('Function ____()', () => {
     suite('American to British English', () => {
 
-      test.skip('Mangoes are my favorite fruit. --> Mangoes are my favourite fruit.', done => {
+      test('Mangoes are my favorite fruit. --> Mangoes are my favourite fruit.', done => {
         const input = 'Mangoes are my favorite fruit.';
         const output = 'Mangoes are my favourite fruit.';
         assert.equal(Translator.toBritish(input).replace(wrapReg, ''), output)
         done();
       });
 
-      test.skip('I ate yogurt for breakfast. --> I ate yoghurt for breakfast.', done => {
+      test('I ate yogurt for breakfast. --> I ate yoghurt for breakfast.', done => {
         const input = 'I ate yogurt for breakfast.';
         const output = 'I ate yoghurt for breakfast.';
 
@@ -46,7 +43,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip("We had a party at my friend's condo. --> We had a party at my friend's flat.", done => {
+      test("We had a party at my friend's condo. --> We had a party at my friend's flat.", done => {
         const input = "We had a party at my friend's condo.";
         const output = "We had a party at my friend's flat.";
 
@@ -54,7 +51,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip('Can you toss this in the trashcan for me? --> Can you toss this in the bin for me?', done => {
+      test('Can you toss this in the trashcan for me? --> Can you toss this in the bin for me?', done => {
         const input = 'Can you toss this in the trashcan for me?';
         const output = 'Can you toss this in the bin for me?';
 
@@ -62,7 +59,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip('The parking lot was full. --> The car park was full.', done => {
+      test('The parking lot was full. --> The car park was full.', done => {
         const input = 'The parking lot was full.';
         const output = 'The car park was full.';
 
@@ -70,7 +67,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip('Like a high tech Rube Goldberg machine. --> Like a high tech Heath Robinson device.', done => {
+      test('Like a high tech Rube Goldberg machine. --> Like a high tech Heath Robinson device.', done => {
         const input = 'Like a high tech Rube Goldberg machine.';
         const output = 'Like a high tech Heath Robinson device.';
 
@@ -78,7 +75,7 @@ suite('Unit Tests', () => {
         done();
       });
       
-      test.skip('To play hooky means to skip class or work. --> To bunk off means to skip class or work.', done => {
+      test('To play hooky means to skip class or work. --> To bunk off means to skip class or work.', done => {
         const input = 'To play hooky means to skip class or work.';
         const output = 'To bunk off means to skip class or work.';
 
@@ -86,7 +83,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip('No Mr. Bond, I expect you to die. --> No Mr Bond, I expect you to die. ', done => {
+      test('No Mr. Bond, I expect you to die. --> No Mr Bond, I expect you to die. ', done => {
         const input = 'No Mr. Bond, I expect you to die.';
         const output = 'No Mr Bond, I expect you to die.';
 
@@ -94,7 +91,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip('Dr. Grosh will see you now. --> Dr Grosh will see you now. ', done => {
+      test('Dr. Grosh will see you now. --> Dr Grosh will see you now. ', done => {
         const input = 'Dr. Grosh will see you now.';
         const output = 'Dr Grosh will see you now.';
 
@@ -102,7 +99,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip('Lunch is at 12:15 today. --> Lunch is at 12.15 today.', done => {
+      test('Lunch is at 12:15 today. --> Lunch is at 12.15 today.', done => {
         const input = 'Lunch is at 12:15 today.';
         const output = 'Lunch is at 12.15 today.';
         
@@ -114,7 +111,7 @@ suite('Unit Tests', () => {
 
     suite('British to American English', () => {
 
-      test.skip('We watched the footie match for a while. --> We watched the soccer match for a while.', done => {
+      test('We watched the footie match for a while. --> We watched the soccer match for a while.', done => {
         const input = 'We watched the footie match for a while.';
         const output = 'We watched the soccer match for a while.';
 
@@ -122,7 +119,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip('Paracetamol takes up to an hour to work. --> Tylenol takes up to an hour to work.', done => {
+      test('Paracetamol takes up to an hour to work. --> Tylenol takes up to an hour to work.', done => {
         const input = 'Paracetamol takes up to an hour to work.';
         const output = 'Tylenol takes up to an hour to work.';
 
@@ -130,7 +127,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip('First, caramelise the onions. --> First, caramelize the onions.', done => {
+      test('First, caramelise the onions. --> First, caramelize the onions.', done => {
         const input = 'First, caramelise the onions.';
         const output = 'First, caramelize the onions.';
 
@@ -138,7 +135,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip('I spent the bank holiday at the funfair. --> I spent the public holiday at the carnival.', done => {
+      test('I spent the bank holiday at the funfair. --> I spent the public holiday at the carnival.', done => {
         const input = 'I spent the bank holiday at the funfair.';
         const output = 'I spent the public holiday at the carnival.';
 
@@ -146,7 +143,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip('I had a bicky then went to the chippy. --> I had a cookie then went to the fish-and-chip shop.', done => {
+      test('I had a bicky then went to the chippy. --> I had a cookie then went to the fish-and-chip shop.', done => {
         const input = 'I had a bicky then went to the chippy.';
         const output = 'I had a cookie then went to the fish-and-chip shop.';
 
@@ -154,7 +151,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip("I've just got bits and bobs in my bum bag. --> I've just got odds and ends in my fanny pack.", done => {
+      test("I've just got bits and bobs in my bum bag. --> I've just got odds and ends in my fanny pack.", done => {
         const input = "I've just got bits and bobs in my bum bag.";
         const output = "I've just got odds and ends in my fanny pack.";
 
@@ -162,7 +159,7 @@ suite('Unit Tests', () => {
         done();
       });
       
-      test.skip("The car boot sale at Boxted Airfield was called off. --> The swap meet at Boxted Airfield was called off.", done => {
+      test("The car boot sale at Boxted Airfield was called off. --> The swap meet at Boxted Airfield was called off.", done => {
         const input = "The car boot sale at Boxted Airfield was called off.";
         const output = "The swap meet at Boxted Airfield was called off.";
 
@@ -170,7 +167,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip("Have you met Mrs Kalyani? --> Have you met Mrs. Kalyani?", done => {
+      test("Have you met Mrs Kalyani? --> Have you met Mrs. Kalyani?", done => {
         const input = "Have you met Mrs Kalyani?";
         const output = "Have you met Mrs. Kalyani?";
 
@@ -178,7 +175,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip("Prof Joyner of King's College, London. --> Prof. Joyner of King's College, London.", done => {
+      test("Prof Joyner of King's College, London. --> Prof. Joyner of King's College, London.", done => {
         const input = "Prof Joyner of King's College, London.";
         const output = "Prof. Joyner of King's College, London.";
 
@@ -186,7 +183,7 @@ suite('Unit Tests', () => {
         done();
       });
 
-      test.skip('Tea time is usually around 4 or 4.30. --> Tea time is usually around 4 or 4:30.', done => {
+      test('Tea time is usually around 4 or 4.30. --> Tea time is usually around 4 or 4:30.', done => {
         const input = 'Tea time is usually around 4 or 4.30.';
         const output = 'Tea time is usually around 4 or 4:30.';
 
